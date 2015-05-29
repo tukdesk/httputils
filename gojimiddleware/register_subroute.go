1a1c21aec0520073c79fef2f8474a8bdf553439a
dtynn <dtynn@163.com>
2015-05-29 12:48:03 +0800

    register subroute example

--- gojimiddleware/register_subroute.go
+++ gojimiddleware/register_subroute.go
@@ -7,6 +7,44 @@ import (
 	"github.com/zenazn/goji/web/middleware"
 )
 
+// RegisterSubroute Example:
+// package main
+
+// import (
+//     "fmt"
+//     "net/http"
+
+//     "github.com/tukdesk/httputils/gojimiddleware"
+//     "github.com/zenazn/goji/web"
+// )
+
+// func hello1(c web.C, w http.ResponseWriter, r *http.Request) {
+//     fmt.Fprint(w, "hello1 _:", c.URLParams)
+//     return
+// }
+
+// func hello2(c web.C, w http.ResponseWriter, r *http.Request) {
+//     fmt.Fprint(w, "hello2 _:", c.URLParams)
+//     return
+// }
+
+// func hello3(c web.C, w http.ResponseWriter, r *http.Request) {
+//     fmt.Fprint(w, "hello3 _:", c.URLParams)
+//     return
+// }
+
+// func main() {
+//     m := web.New()
+//     m.Handle("", hello1)
+//     m.Handle("/abc/*", hello2)
+//     m.Handle("/*", hello3)
+
+//     app := web.New()
+
+//     gojimiddleware.RegisterSubroute("/hello", app, m)
+//     http.ListenAndServe(":50001", app)
+// }
+
 func RegisterSubroute(prefix string, app, mod *web.Mux) {
 	str := "^" + prefix + "(?P<_>(/.*)?)$"
 	mod.Use(middleware.SubRouter)
