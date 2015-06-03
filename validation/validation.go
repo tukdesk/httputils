6394bdfc7830f6bcd67f9562d411a51a64841798
dtynn <dtynn@163.com>
2015-06-03 14:10:16 +0800

    validation

---
+++ validation/validation.go
@@ -0,0 +1,151 @@
+package validation
+
+import (
+	"fmt"
+)
+
+type ValidationError struct {
+	Key     string
+	Message string
+}
+
+func (this *ValidationError) Error() string {
+	return this.Message
+}
+
+type Validation struct {
+	errors []*ValidationError
+	multi  bool
+}
+
+func New() *Validation {
+	return &Validation{
+		errors: make([]*ValidationError, 1),
+		multi:  false,
+	}
+}
+
+func NewMulti() *Validation {
+	return &Validation{
+		errors: make([]*ValidationError, 0),
+		multi:  true,
+	}
+}
+
+func (this *Validation) FirstError() error {
+	if len(this.errors) == 0 {
+		return nil
+	}
+	return this.errors[0]
+}
+
+func (this *Validation) addError(key, message string) {
+	this.errors = append(this.errors, &ValidationError{
+		Key:     key,
+		Message: message,
+	})
+	return
+}
+
+func (this *Validation) should() bool {
+	if this.multi {
+		return true
+	}
+	return len(this.errors) == 0
+}
+
+func (this *Validation) Required(key string, obj interface{}) {
+	if !this.should() {
+		return
+	}
+
+	valid := ValidatorRequired(obj)
+	if !valid {
+		message := fmt.Sprintf("%s is required", key)
+		this.addError(key, message)
+	}
+}
+
+func (this *Validation) MinSize(key string, obj interface{}, min int) {
+	if !this.should() {
+		return
+	}
+
+	valid := ValidatorMinSize(obj, min)
+	if !valid {
+		message := fmt.Sprintf("minimum length of %s is %d", key, min)
+		this.addError(key, message)
+	}
+}
+
+func (this *Validation) MaxSize(key string, obj interface{}, max int) {
+	if !this.should() {
+		return
+	}
+
+	valid := ValidatorMaxSize(obj, max)
+	if !valid {
+		message := fmt.Sprintf("maximum length of %s is %d", key, max)
+		this.addError(key, message)
+	}
+}
+
+func (this *Validation) Min(key string, obj interface{}, min int) {
+	if !this.should() {
+		return
+	}
+
+	valid := ValidatorMin(obj, min)
+	if !valid {
+		message := fmt.Sprintf("minimum value of %s is %d", key, min)
+		this.addError(key, message)
+	}
+}
+
+func (this *Validation) Max(key string, obj interface{}, max int) {
+	if !this.should() {
+		return
+	}
+
+	valid := ValidatorMax(obj, max)
+	if !valid {
+		message := fmt.Sprintf("maximum value of %s is %d", key, max)
+		this.addError(key, message)
+	}
+}
+
+func (this *Validation) Email(key, email string) {
+	if !this.should() {
+		return
+	}
+
+	valid := ValidatorEmail(email)
+	if !valid {
+		message := fmt.Sprintf("value of %s is not a valid email address", key)
+		this.addError(key, message)
+	}
+}
+
+func (this *Validation) In(key string, obj interface{}, options []interface{}) {
+	if !this.should() {
+		return
+	}
+
+	valid := ValidatorIn(obj, options)
+	if !valid {
+		message := fmt.Sprintf("value of %s is not in given options", key)
+		this.addError(key, message)
+	}
+}
+
+func (this *Validation) Range(key string, obj interface{}, min, max int) {
+	if !this.should() {
+		return
+	}
+
+	valid := ValidatorRange(obj, min, max)
+	if !valid {
+		message := fmt.Sprintf("value of %s shoud be in range [%d, %d]", key, min, max)
+		this.addError(key, message)
+	}
+}
---
+++ validation/validators.go
@@ -0,0 +1,96 @@
+package validation
+
+import (
+	"reflect"
+	"regexp"
+	"unicode/utf8"
+)
+
+var reMail = regexp.MustCompile("[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[a-zA-Z0-9](?:[\\w-]*[\\w])?")
+var reSlug = regexp.MustCompile("^[a-zA-Z0-9][\\w-]*$")
+
+func ValidatorRequired(obj interface{}) bool {
+	if obj == nil {
+		return false
+	}
+	if str, ok := obj.(string); ok {
+		return str != ""
+	}
+	if b, ok := obj.(bool); ok {
+		return b
+	}
+	if i, ok := obj.(int); ok {
+		return i != 0
+	}
+	if i, ok := obj.(int64); ok {
+		return i != 0
+	}
+	v := reflect.ValueOf(obj)
+	if v.Kind() == reflect.Slice {
+		return v.Len() > 0
+	}
+	return true
+}
+
+func ValidatorEmail(email string) bool {
+	return reMail.MatchString(email)
+}
+
+func ValidatorMinSize(obj interface{}, min int) bool {
+	if str, ok := obj.(string); ok {
+		return utf8.RuneCountInString(str) >= min
+	}
+	v := reflect.ValueOf(obj)
+	if v.Kind() == reflect.Slice {
+		return v.Len() >= min
+	}
+	return false
+}
+
+func ValidatorMaxSize(obj interface{}, max int) bool {
+	if str, ok := obj.(string); ok {
+		return utf8.RuneCountInString(str) <= max
+	}
+	v := reflect.ValueOf(obj)
+	if v.Kind() == reflect.Slice {
+		return v.Len() <= max
+	}
+	return false
+}
+
+func ValidatorMin(obj interface{}, min int) bool {
+	if num, ok := obj.(int); ok {
+		return num >= min
+	}
+	if num, ok := obj.(int64); ok {
+		return num >= int64(min)
+	}
+	return false
+}
+
+func ValidatorMax(obj interface{}, max int) bool {
+	if num, ok := obj.(int); ok {
+		return num <= max
+	}
+	if num, ok := obj.(int64); ok {
+		return num <= int64(max)
+	}
+	return false
+}
+
+func ValidatorMatch(re *regexp.Regexp, s string) bool {
+	return re.MatchString(s)
+}
+
+func ValidatorIn(obj interface{}, options []interface{}) bool {
+	for _, one := range options {
+		if obj == one {
+			return true
+		}
+	}
+	return false
+}
+
+func ValidatorRange(obj interface{}, min, max int) bool {
+	return ValidatorMin(obj, min) && ValidatorMax(obj, max)
+}
