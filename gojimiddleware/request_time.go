7768921298f5a94e4ef45114f9aa61b9f3d0d00f
dtynn <dtynn@163.com>
2015-06-26 01:40:06 +0800

    fix

--- gojimiddleware/request_time.go
+++ gojimiddleware/request_time.go
@@ -1,7 +1,6 @@
 package gojimiddleware
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
@@ -17,39 +16,14 @@ func RequestTimer(c *web.C, h http.Handler) http.Handler {
 
 		start := time.Now()
 		h.ServeHTTP(lw, r)
+		stop := time.Now()
 
 		if lw.Status() == 0 {
 			lw.WriteHeader(http.StatusOK)
 		}
 
-		logger.SimpleInfof("[%d] %s | %s | %s", lw.Status(), r.Method, r.URL.String(), SinceStr(start))
+		logger.SimpleInfof("[%d] %s %s | %s | %d", lw.Status(), r.Method, r.URL.String(), stop.Sub(start), lw.BytesWritten())
 	}
 
 	return http.HandlerFunc(fn)
-}
-
-var (
-	intSecond      = int64(time.Second)
-	intMillisecond = int64(time.Millisecond)
-	intMicrosecond = int64(time.Microsecond)
-	intNanosecond  = int64(time.Nanosecond)
-
-	floatSecond      = float64(time.Second)
-	floatMillisecond = float64(time.Millisecond)
-	floatMicrosecond = float64(time.Microsecond)
-	floatNanosecond  = float64(time.Nanosecond)
-)
-
-func SinceStr(then time.Time) string {
-	duration := time.Now().UnixNano() - then.UnixNano()
-	switch {
-	case duration > intSecond:
-		return fmt.Sprintf("%.2f s", float64(duration)/floatSecond)
-	case duration > intMillisecond:
-		return fmt.Sprintf("%.2f ms", float64(duration)/floatMillisecond)
-	case duration > intMicrosecond:
-		return fmt.Sprintf("%.2f us", float64(duration)/floatMicrosecond)
-	default:
-		return fmt.Sprintf("%.2f ns", float64(duration)/floatNanosecond)
-	}
 }
