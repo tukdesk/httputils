2d7b707e23d4389524064300a85b2606704e93f6
dtynn <dtynn@163.com>
2015-07-31 10:51:10 +0800

    xlogger

--- echomiddleware/request_logger.go
+++ echomiddleware/request_logger.go
@@ -18,10 +18,14 @@ func RequestLogger() func(*echo.Context) error {
 }
 
 func GetRequestLogger(c *echo.Context) *xlogger.XLogger {
+	// Context 被复用, store 中已有 logger, reset 后直接返回
+
 	if logger, ok := c.Get(RequestLoggerKey).(*xlogger.XLogger); ok {
+		logger.ResetForRequest(c.Response().Writer(), c.Request())
 		return logger
 	}
 
+	// Context 为新创建的对象, set 一个 logger 到 store 中
 	logger := xlogger.NewXLogger(c.Response().Writer(), c.Request())
 	c.Set(RequestLoggerKey, logger)
 
--- xlogger/xlogger.go
+++ xlogger/xlogger.go
@@ -40,141 +40,155 @@ func genReqId() string {
 }
 
 type XLogger struct {
-	nLogger            *log.Logger
-	sLogger            *log.Logger
-	reqId, reqIdLogStr string
+	reqId string
+	r     *http.Request
 }
 
 func NewXLogger(w http.ResponseWriter, r *http.Request) *XLogger {
 	reqId := genReqId()
 	w.Header().Set("X-Req-Id", reqId)
 	return &XLogger{
-		nLogger: normalLogger,
-		sLogger: simpleLogger,
-		reqId:   reqId,
+		reqId: reqId,
+		r:     r,
 	}
 }
 
 func (this *XLogger) Print(v ...interface{}) {
-	this.nLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprint(v...))
+	normalLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprint(v...))
 }
 
 func (this *XLogger) Println(v ...interface{}) {
-	this.nLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprintln(v...))
+	normalLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprintln(v...))
 }
 
 func (this *XLogger) Printf(format string, v ...interface{}) {
-	this.nLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprintf(format, v...))
+	normalLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprintf(format, v...))
 }
 
 func (this *XLogger) Debug(v ...interface{}) {
-	if this.nLogger.Level > log.Ldebug {
+	if normalLogger.Level > log.Ldebug {
 		return
 	}
-	this.nLogger.Output(this.reqId, log.Ldebug, 2, fmt.Sprintln(v...))
+	normalLogger.Output(this.reqId, log.Ldebug, 2, fmt.Sprintln(v...))
 }
 
 func (this *XLogger) Debugf(format string, v ...interface{}) {
-	if this.nLogger.Level > log.Ldebug {
+	if normalLogger.Level > log.Ldebug {
 		return
 	}
-	this.nLogger.Output(this.reqId, log.Ldebug, 2, fmt.Sprintf(format, v...))
+	normalLogger.Output(this.reqId, log.Ldebug, 2, fmt.Sprintf(format, v...))
 }
 
 func (this *XLogger) Info(v ...interface{}) {
-	if this.nLogger.Level > log.Linfo {
+	if normalLogger.Level > log.Linfo {
 		return
 	}
-	this.nLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprintln(v...))
+	normalLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprintln(v...))
 }
 
 func (this *XLogger) Infof(format string, v ...interface{}) {
-	if this.nLogger.Level > log.Linfo {
+	if normalLogger.Level > log.Linfo {
 		return
 	}
-	this.nLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprintf(format, v...))
+	normalLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprintf(format, v...))
 }
 
 func (this *XLogger) Warn(v ...interface{}) {
-	if this.nLogger.Level > log.Lwarn {
+	if normalLogger.Level > log.Lwarn {
 		return
 	}
-	this.nLogger.Output(this.reqId, log.Lwarn, 2, fmt.Sprintln(v...))
+	normalLogger.Output(this.reqId, log.Lwarn, 2, fmt.Sprintln(v...))
 }
 
 func (this *XLogger) Warnf(format string, v ...interface{}) {
-	if this.nLogger.Level > log.Lwarn {
+	if normalLogger.Level > log.Lwarn {
 		return
 	}
-	this.nLogger.Output(this.reqId, log.Lwarn, 2, fmt.Sprintf(format, v...))
+	normalLogger.Output(this.reqId, log.Lwarn, 2, fmt.Sprintf(format, v...))
 }
 
 func (this *XLogger) Error(v ...interface{}) {
-	this.nLogger.Output(this.reqId, log.Lerror, 2, fmt.Sprintln(v...))
+	normalLogger.Output(this.reqId, log.Lerror, 2, fmt.Sprintln(v...))
 }
 
 func (this *XLogger) Errorf(format string, v ...interface{}) {
-	this.nLogger.Output(this.reqId, log.Lerror, 2, fmt.Sprintf(format, v...))
+	normalLogger.Output(this.reqId, log.Lerror, 2, fmt.Sprintf(format, v...))
 }
 
 func (this *XLogger) SimplePrint(v ...interface{}) {
-	this.sLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprint(v...))
+	simpleLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprint(v...))
 }
 
 func (this *XLogger) SimplePrintln(v ...interface{}) {
-	this.sLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprintln(v...))
+	simpleLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprintln(v...))
 }
 
 func (this *XLogger) SimplePrintf(format string, v ...interface{}) {
-	this.sLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprintf(format, v...))
+	simpleLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprintf(format, v...))
 }
 
 func (this *XLogger) SimpleDebug(v ...interface{}) {
-	if this.sLogger.Level > log.Ldebug {
+	if simpleLogger.Level > log.Ldebug {
 		return
 	}
-	this.sLogger.Output(this.reqId, log.Ldebug, 2, fmt.Sprintln(v...))
+	simpleLogger.Output(this.reqId, log.Ldebug, 2, fmt.Sprintln(v...))
 }
 
 func (this *XLogger) SimpleDebugf(format string, v ...interface{}) {
-	if this.sLogger.Level > log.Ldebug {
+	if simpleLogger.Level > log.Ldebug {
 		return
 	}
-	this.sLogger.Output(this.reqId, log.Ldebug, 2, fmt.Sprintf(format, v...))
+	simpleLogger.Output(this.reqId, log.Ldebug, 2, fmt.Sprintf(format, v...))
 }
 
 func (this *XLogger) SimpleInfo(v ...interface{}) {
-	if this.sLogger.Level > log.Linfo {
+	if simpleLogger.Level > log.Linfo {
 		return
 	}
-	this.sLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprintln(v...))
+	simpleLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprintln(v...))
 }
 
 func (this *XLogger) SimpleInfof(format string, v ...interface{}) {
-	if this.sLogger.Level > log.Linfo {
+	if simpleLogger.Level > log.Linfo {
 		return
 	}
-	this.sLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprintf(format, v...))
+	simpleLogger.Output(this.reqId, log.Linfo, 2, fmt.Sprintf(format, v...))
 }
 
 func (this *XLogger) SimpleWarn(v ...interface{}) {
-	if this.sLogger.Level > log.Lwarn {
+	if simpleLogger.Level > log.Lwarn {
 		return
 	}
-	this.sLogger.Output(this.reqId, log.Lwarn, 2, fmt.Sprintln(v...))
+	simpleLogger.Output(this.reqId, log.Lwarn, 2, fmt.Sprintln(v...))
 }
 
 func (this *XLogger) SimpleWarnf(format string, v ...interface{}) {
-	if this.sLogger.Level > log.Lwarn {
+	if simpleLogger.Level > log.Lwarn {
 		return
 	}
-	this.sLogger.Output(this.reqId, log.Lwarn, 2, fmt.Sprintf(format, v...))
+	simpleLogger.Output(this.reqId, log.Lwarn, 2, fmt.Sprintf(format, v...))
 }
 
 func (this *XLogger) SimpleError(v ...interface{}) {
-	this.sLogger.Output(this.reqId, log.Lerror, 2, fmt.Sprintln(v...))
+	simpleLogger.Output(this.reqId, log.Lerror, 2, fmt.Sprintln(v...))
 }
 
 func (this *XLogger) SimpleErrorf(format string, v ...interface{}) {
-	this.sLogger.Output(this.reqId, log.Lerror, 2, fmt.Sprintf(format, v...))
+	simpleLogger.Output(this.reqId, log.Lerror, 2, fmt.Sprintf(format, v...))
+}
+
+func (this *XLogger) Request() *http.Request {
+	return this.r
+}
+
+func (this *XLogger) ResetForRequest(w http.ResponseWriter, r *http.Request) {
+	if this.r == r {
+		return
+	}
+
+	reqId := genReqId()
+	w.Header().Set("X-Req-Id", reqId)
+	this.reqId = reqId
+	this.r = r
+	return
 }
